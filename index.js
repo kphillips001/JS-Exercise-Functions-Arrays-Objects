48dd6865efcfe9dbfccae651d5cfb4f79fe67dad
Kevin <knphillips01@gmail.com>
2019-12-16 14:52:11 -0600

    finished getGermanCars

--- index.js
+++ index.js
@@ -223,7 +223,7 @@ function getModelYears(inventory) {
  * with a `car_year` which is at most the given desired max year,
  * in the same order as they appear in the original inventory.
 */
-function getOlderCars(/* code here */) {
+function getOlderCars() {
   /* code here */
 }
 
@@ -238,8 +238,16 @@ function getOlderCars(/* code here */) {
  * made by either `Audi` or `Mercedes-Benz` or `Volkswagen` or `BMW`,
  * in the same order as they appear in the original inventory.
 */
-function getGermanCars(/* code here */) {
-  /* code here */
+function getGermanCars(inventory) {
+  let germanCars = [];
+  let germanModels = [`Audi`, `Mercedes-Benz`, `Volkswagen`, `BMW`];
+
+  for (let i = 0; i < inventory.length; i++) {
+    if (germanModels.includes(inventory[i].car_make)) {
+      germanCars.push(inventory[i])
+    }
+  }
+  return germanCars;
 }
 
 /**
