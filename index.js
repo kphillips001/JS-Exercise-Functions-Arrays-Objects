1972070ad1096e2be1c6df72fe343e2d3a6bb983
Kevin <knphillips01@gmail.com>
2019-12-16 15:33:03 -0600

    finished sortCarInventory

--- index.js
+++ index.js
@@ -191,8 +191,14 @@ function getCarInfoById(inventory, id) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * sortCarInventory returns an inventory that is sorted by car_model, ascending [A-Z].
 */
-function sortCarInventory(/* code here */) {
-  /* code here */
+function sortCarInventory(inventory) {
+  return inventory.sort((a, b) => {
+    if (a.car_model > b.car_model) {
+      return 1
+    } else {
+      return -1
+    }
+  })
 }
 
 /**
